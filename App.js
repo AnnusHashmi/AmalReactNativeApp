042d4b7a9532674056eb7d025e32fc38b22d03e1
Annus Hashmi <annus.hashmi29@gmail.com>
2021-05-20 23:42:19 +0500

    HomeScreen completed, routing completed

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import HomeScreen from './Screens/HomeScreen/HomeScreen'
+import Hospitals from './Screens/Hospitals/Hospitals'
+import IsolationVaccination from './Screens/IsolationCenters/Isolations'
+import Symptoms from './Screens/Symptoms/Symptoms'
+import AtHomePreventions from './Screens/AtHome/AtHome'
+import CenterHospitals from './Screens/Hospitals/Center/CenterHospitals'
+import EastHospitals from './Screens/Hospitals/East/EastHospitals'
+import WestHopsitals from './Screens/Hospitals/West/WestHospitals'
+import DistrictMalirHospitals from './Screens/Hospitals/DistricMalir/DistrictMalirHospitals'
+import SouthHospitals from './Screens/Hospitals/South/SouthHospitals'
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>This is Annus Hashmi</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+      <Stack.Navigator screenOptions={{
+        header : () => null,
+      }}
+      >
+        <Stack.Screen name='Main' component={HomeScreen} />
+        <Stack.Screen name='Hospitals' component={Hospitals} />
+        <Stack.Screen name='Isolations' component={IsolationVaccination} />
+        <Stack.Screen name='Symptoms' component={Symptoms} />
+        <Stack.Screen name='At Home Prevention' component={AtHomePreventions} />
+        <Stack.Screen name='CenterHospitals' component={CenterHospitals} />
+        <Stack.Screen name='EastHospitals' component={EastHospitals} />
+        <Stack.Screen name='WestHospitals' component={WestHopsitals} />
+        <Stack.Screen name='DistrictMalirHospitals' component={DistrictMalirHospitals} />
+        <Stack.Screen name='SouthHospitals' component={SouthHospitals} />
+
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <HomeScreen />
+    // </View>
+    
   );
 }
 
